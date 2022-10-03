b6e491e48d3cb96aa5f087c738eb22cbe97e3b0b
Darian Chen <darian.chen8639@gmail.com>
2022-10-03 15:16:37 -0700

    Add nav component WIP

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import Header from "./components/Header/Header";
+import SimpleBottomNavigation from "./components/nav";
 
 function App() {
   return (
-   <>
-    <Header />
-    <div className="app">testing testing</div>
-  </>
+    <>
+      <Header />
+      <div className="app">testing testing</div>
+      <SimpleBottomNavigation />
+    </>
   );
 }
 
