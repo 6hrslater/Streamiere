56bbacf5a7db6af6b4843b33131f3c5257a9ab4e
Darian Chen <darian.chen8639@gmail.com>
2022-10-03 16:57:32 -0700

    Add routes to app.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
+import { BrowserRouter } from "react-router-dom/cjs/react-router-dom.min";
 import "./App.css";
 import Header from "./components/Header/Header";
 import SimpleBottomNavigation from "./components/nav";
 
 function App() {
   return (
-    <>
+    <BrowserRouter>
       <Header />
-      <div className="app">testing testing</div>
+      <div className="app"></div>
+      <Container>
+        <Switch>
+          <Route path="/" component={Trending} exact />
+          <Route path="/movies" component={Movies} />
+          <Route path="/series" component={Series} />
+          <Route path="/search" component={Search} />
+        </Switch>
+      </Container>
       <SimpleBottomNavigation />
-    </>
+    </BrowserRouter>
   );
 }
 
