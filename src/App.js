8755934c989f7d901b5641489d663a49c2835dff
Woncheol Kim <icijiy@gmail.com>
2022-10-03 13:01:45 -0700

    Change font style

--- src/App.js
+++ src/App.js
@@ -2,9 +2,10 @@ import Header from "./components/Header/Header";
 
 function App() {
   return (
-  <div className="App">
+   <>
     <Header />
-  </div>
+    <div className="App"></div>
+  </>
   );
 }
 
