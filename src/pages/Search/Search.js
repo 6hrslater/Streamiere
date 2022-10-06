19085b74dbbb059c0be50d4144aafb9c441f421a
Woncheol Kim <icijiy@gmail.com>
2022-10-05 22:11:06 -0700

    fixed errors

--- src/pages/Search/Search.js
+++ src/pages/Search/Search.js
@@ -32,7 +32,8 @@ const Search = () => {
   const fetchSearch = async () => {
     try {
       const { data } = await axios.get(
-        `https://api.themoviedb.org/3/search/${type ? "tv" : "movie"}?api_key=${process.env.REACT_APP_API_KEY
+        `https://api.themoviedb.org/3/search/${type ? "tv" : "movie"}?api_key=${
+          process.env.REACT_APP_API_KEY
         }&language=en-US&query=${searchText}&page=${page}&include_adult=false`
       );
       setContent(data.results);
