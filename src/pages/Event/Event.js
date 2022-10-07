cc3d8ce5b5d6b66b782266b1e9e5021e30483e6b
Woncheol Kim <icijiy@gmail.com>
2022-10-07 12:33:41 -0700

    Update Google map with Marker

--- src/pages/Event/Event.js
+++ src/pages/Event/Event.js
@@ -1,20 +1,32 @@
-import { withGoogleMap, GoogleMap, Marker } from "react-google-maps"
+import {
+  withScriptjs,
+  withGoogleMap,
+  GoogleMap,
+  Marker,
+} from "react-google-maps";
 
-const MyMapComponent = withGoogleMap((props) =>
-  <GoogleMap
-    defaultZoom={8}
-    defaultCenter={{ lat: -34.397, lng: 150.644 }}
-  >
-    {props.isMarkerShown && <Marker position={{ lat: -34.397, lng: 150.644 }} />}
-  </GoogleMap>
-)
+const MyMapComponent = withScriptjs(
+  withGoogleMap((props) => (
+    <GoogleMap defaultZoom={8} defaultCenter={{ lat: 49.285782896422546, lng: -123.12319139162997 }}>
+      {props.isMarkerShown && (
+        <Marker position={{ lat: 49.28232859565747, lng: -123.12433441038579 }} />
+      )}
+    </GoogleMap>
+  ))
+);
  
  
 
 const Event = () => {
   return (
     <>
- <MyMapComponent isMarkerShown />
+      <MyMapComponent
+        isMarkerShown
+        googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places"
+        loadingElement={<div style={{ height: `100%` }} />}
+        containerElement={<div style={{ height: `400px` }} />}
+        mapElement={<div style={{ height: `100%` }} />}
+      />
     </>
   );
 };
