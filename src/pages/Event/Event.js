079616fe0ef28512f711b982e3a4909a2c146845
Woncheol Kim <icijiy@gmail.com>
2022-10-08 23:31:37 -0700

    Add mapdata function

--- src/pages/Event/Event.js
+++ src/pages/Event/Event.js
@@ -3,31 +3,18 @@ import {
   withGoogleMap,
   GoogleMap,
   Marker,
+  InfoWindow,
 } from "react-google-maps";
 import axios from "axios";
-import React from "react";
+import React, { useState } from "react";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import "./Event.css";
-import {CalendarComponent} from '@syncfusion/ej2-react-calendars'
+import { CalendarComponent } from "@syncfusion/ej2-react-calendars";
+import "./default-style.css";
 
-// import ApiCalendar from "react-google-calendar-api";
-
-const MyMapComponent = withScriptjs(
-  withGoogleMap((props) => (
-    <GoogleMap
-      defaultZoom={10}
-      defaultCenter={{ lat: 49.285782896422546, lng: -123.12319139162997 }}
-    >
-      {props.isMarkerShown && (
-        <Marker
-          position={{ lat: 49.28232859565747, lng: -123.12433441038579 }}
-        />
-      )}
-    </GoogleMap>
-  ))
-);
+const mapData = [{ id: 1, lat: 49.28015560764768, lng: -123.1124679569322 }];
 
 function PosterSlider() {
   var settings = {
@@ -46,19 +33,49 @@ function PosterSlider() {
         <img alt="lionking" src="/posters/lionking.jpeg"></img>
       </div>
       <div className="poster">
-        <img alt="jurassicpark" src="/posters/jurassicpark.jpeg"></img>    
+        <img alt="jurassicpark" src="/posters/jurassicpark.jpeg"></img>
       </div>
       <div className="poster">
         <img alt="jumanji" src="/posters/jumanji.jpeg"></img>
       </div>
-  </Slider>
+    </Slider>
   );
 }
 
 const Event = () => {
-  const dateValue: Date = new Date(new Date().getFullYear(), new Date().getMonth(), 10);
-  const startDate: Date = new Date(new Date().getFullYear(), new Date().getMonth(), 6);
-  const endDate: Date = new Date(new Date().getFullYear(), new Date().getMonth(), 25);
+  const [infoBox, setInfoBox] = useState(null);
+
+  const MyMapComponent = withScriptjs(
+    withGoogleMap((props) => (
+      <GoogleMap
+        defaultZoom={10}
+        defaultCenter={{ lat: 49.285782896422546, lng: -123.12319139162997 }}
+      >
+        {mapData.map((location) => (
+          <Marker
+            key={location.id}
+            position={{
+              lat: location.lat,
+              lng: location.lng,
+            }}
+          />
+        ))}
+      </GoogleMap>
+    ))
+  );
+
+  const dateValue = new Date(
+    new Date().getFullYear(),
+    new Date().getMonth(),
+    10
+  );
+  const startDate = new Date(
+    new Date().getFullYear(),
+    new Date().getMonth(),
+    6
+  );
+  const endDate = new Date(new Date().getFullYear(), new Date().getMonth(), 25);
+
   const handleSMS = () => {
     axios.get("http://127.0.0.1:3001/sendSMS");
   };
@@ -69,16 +86,16 @@ const Event = () => {
       <button onClick={handleSMS}>Send SMS</button>
 
       <div style={{ width: "95vw", height: "80vh" }}>
-      <div>
-      <CalendarComponent 
-      value={dateValue} 
-      min={startDate} 
-      max={endDate}
-      isMultiSelection={true}
-      start="Month"
-      depth="Week"> 
-      </CalendarComponent>
-      </div>
+        <div>
+          <CalendarComponent
+            value={dateValue}
+            min={startDate}
+            max={endDate}
+            isMultiSelection={true}
+            start="Month"
+            depth="Week"
+          ></CalendarComponent>
+        </div>
         <MyMapComponent
           isMarkerShown
           googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=${process.env.REACT_APP_GOOGLE_KEY}`}
@@ -90,16 +107,5 @@ const Event = () => {
     </>
   );
 };
-
-// const apiCalendar = new ApiCalendar(config);
-
-// const config = {
-//   clientId: "<CLIENT_ID>",
-//   apiKey: "<API_KEY>",
-//   scope: "https://www.googleapis.com/auth/calendar",
-//   discoveryDocs: [
-//     "https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest",
-//   ],
-// };
 
 export default Event;
